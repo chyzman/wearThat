f9dc3c4fbb4ef0b8824ebd07c23ad4e4fef93842
chyzman <32403637+chyzman@users.noreply.github.com>
2024-07-16 17:49:16 -0400

    1.21

--- src/main/java/com/chyzman/wearthat/mixin/ArmorSlotMixin.java
+++ src/main/java/com/chyzman/wearthat/mixin/ArmorSlotMixin.java
@@ -1,15 +1,16 @@
 package com.chyzman.wearthat.mixin;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.screen.slot.ArmorSlot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(targets = {"net/minecraft/screen/PlayerScreenHandler$1"})
-public class PlayerScreenHandlerSlotMixin {
+@Mixin(ArmorSlot.class)
+public class ArmorSlotMixin {
 
-    @Inject(method = "canInsert(Lnet/minecraft/item/ItemStack;)Z", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "canInsert", at = @At("RETURN"), cancellable = true)
     public void allowItemEquipping(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(true);
     }
